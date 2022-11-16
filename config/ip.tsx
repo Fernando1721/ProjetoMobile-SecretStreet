0e25a899a1843702d5ffc1b7388e5411231b74b0
Renan Lazoti <renanlazoti@gmail.com>
2022-11-16 14:46:13 -0300

    Tudo

---
+++ config/ip.tsx
@@ -0,0 +1,2 @@
+export const ipnode = "http://10.26.49.33:8080";
+export const ipspring = "http://10.26.49.33:8085";
---
+++ screens/Detalhes/css/Styles.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    backgroundColor: "black",
+    justifyContent: "space-between",
+    alignItems: "center",
+    flex: 2,
+    padding: 10,
+    paddingTop: 20,
+  },
+  logo: {
+    width: 50,
+    height: 50,
+    resizeMode: "cover",
+  },
+  titulo: {
+    fontSize: 20,
+    color: "white",
+    fontweight: "bold",
+  },
+
+  btncarrinho: {
+    padding:20,
+    backgroundColor: "red",
+    margin: 20,
+    borderRadius: 15,
+    marginLeft: "auto",
+    marginRight: "auto"
+  },
+
+  txtcarrinho: {
+    color: "#fff",
+    fontSize: 15,
+    fontWeight: "bold",
+    textAlign: "center",
+  }
+});
